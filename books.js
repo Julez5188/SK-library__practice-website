4c78261e472bddc628c1d1139e2230371ec8777e
Julia Maximov <julia.maximov18@gmail.com>
2026-09-09 21:54:37 -0400

    index.html addeded menu for small screens 9/9/26

---
+++ books.js
@@ -0,0 +1,8 @@
+// https://stephen-king-api.onrender.com/api/books //
+
+async function main() {
+  const users = await fetch(https://stephen-king-api.onrender.com/api/books);
+  const usersData = await users.json();
+  const userList = document.querySelector
+  console.log(userList)
+}
