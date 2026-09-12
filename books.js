fbb1310dd9e46ada6d73d8063a20f6202a5154a1
Julia Maximov <julia.maximov18@gmail.com>
2026-09-11 23:28:53 -0400

    bug fixes and adding data to the books 9/11/26

--- books.js
+++ books.js
@@ -1,27 +1,72 @@
 // https://stephen-king-api.onrender.com/api/books //
 const bookExtras = {
   "Carrie": {
-    image: "./assets/carrie-img.jpg", 
-    price: "$19.95",
+    image: "./assets/carrie-img.jpg",
+    price: "$19.95"
+  },
+  "Salem's Lot": {
+    image: "./assets/salems-lot-jpg.jpg",
+    price: "$29.95"
+  },
+  "The Shining": {
+    image: "./assets/the-shining-jpg.jpg",
+    price: "$59.95"
+  },
+  "Rage": {
+    image: "./assets/rage-img-jpg.jpg",
+    price: "$9.99"
+  },
+  "The Stand": {
+    image: "./assets/the-stand-jpg.jpg",
+    price: "$29.95"
+  },
+  "The Long Walk": {
+    image: "./assets/the-long-walk-jpg.jpg",
+    price: "$9.99"
+  },
+  "The Dead Zone": {
+    image: "./assets/the-dead-zone.jpg",
+    price: "$19.95"
+  },
+  "Firestarter": {
+    image: "./assets/firestarter-img.jpg",
+    price: "$34.95"
+  },
+  "Roadwork": {
+    image: "./assets/road-work.jpg",
+    price: "$45.99"
+  },
+  "Cujo": {
+    image: "./assets/cujo.jpg",
+    price: "$29.99"
+  },
+  "Christine": {
+    image: "./assets/christine-img.jpg",
+    price: "$15.95"
+  },
+  "Pet Sematary": {
+    image: "./assets/pet-sematary.jpg",
+    price: "$45.99"
   }
+
 }
 
 async function main() {
   const books = await fetch("https://stephen-king-api.onrender.com/api/books");
-  const booksData = (await books.json()).data;
+  const response = (await books.json()).data;
   const bookListEl = document.querySelector(".books");
   
 
-  bookListEl.innerHTML = booksData
+  bookListEl.innerHTML = response
   .map(
       (book) => {
         const extras = bookExtras[book.Title] || {
           image: "./assets/default.jpg",
-          price: "N/A",
+          price: "N/A"
         }
       return `<div class="book">
               <figure class="book__img--wrapper">
-                ${extras.image}
+                <img src="${extras.image}" alt="">
               </figure>
               <div class="book__title">
                 ${book.Title}
@@ -30,7 +75,7 @@ async function main() {
                 ${book.Year}
               </div>
               <div class="book__price">
-                <span class="book__price">${bookExtras.price}</span>
+                <span class="book__price">${extras.price}</span>
               </div>
             </div>`}
           )
